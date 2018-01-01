0d6939b168023ed8ded2892499e62dd68a969c7e
Ntim Yeboah <lylegendary@gmail.com>
2018-01-01 12:13:19 +0000

    Apply fixes from StyleCI

--- tests/Unit/DraftTest.php
+++ tests/Unit/DraftTest.php
@@ -9,11 +9,11 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class DraftTest extends TestCase
 {
     use RefreshDatabase;
-    
+
     public function test_draft_is_published()
     {
         $draft = factory(Draft::class)->create([
-            'is_published' => 1
+            'is_published' => 1,
         ]);
 
         $this->assertTrue($draft->isPublished());
